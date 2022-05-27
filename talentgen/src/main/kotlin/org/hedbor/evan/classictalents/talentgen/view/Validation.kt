b78bd32ca9380e6bd7d44ec88ebdc53983a81a23
Evan Hedbor <evan@hedbor.org>
2022-05-27 17:43:32 +0200

    Update copyright

--- talentgen/src/main/kotlin/org/hedbor/evan/classictalents/talentgen/view/Validation.kt
+++ talentgen/src/main/kotlin/org/hedbor/evan/classictalents/talentgen/view/Validation.kt
@@ -1,6 +1,6 @@
 /*
  * WoW Classic Talent Calculator
- * Copyright (C) 2020-2021 Evan Hedbor
+ * Copyright (C) 2020-2022 Evan Hedbor
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
